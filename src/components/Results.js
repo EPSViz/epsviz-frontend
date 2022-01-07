cd2680e70af0d70193d2059b6b04ba9a5a3a07e1
yokester98 <yoke1998@gmail.com>
2022-01-08 04:22:45 +0800

    Update Results.js

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,9 @@
 import { Card, Typography } from "@mui/material";
 
 export default function Results() {
+  // need to link to predicted EPS later...
+  let prediction = 3;
+  let actual = 2;
   return (
     <div>
       <Card
@@ -12,8 +15,15 @@ export default function Results() {
           justifyContent: "space-around",
         }}
       >
-        <Typography sx={{my: 3}} variant="h5">r<sup>2</sup>:</Typography>
-        <Typography sx={{my: 3}} variant="h5">p-value: </Typography>
+        <Typography sx={{ my: 3 }} variant="h5">
+          r<sup>2</sup>:
+        </Typography>
+        <Typography sx={{ my: 3 }} variant="h5">
+          p-value:{" "}
+        </Typography>
+        <Typography sx={{ my: 3 }} variant="h5">
+          {prediction > actual ? "Beat" : "Miss"}
+        </Typography>
       </Card>
     </div>
   );
